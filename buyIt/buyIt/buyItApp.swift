4e21ed18f7208a36c771cb8c02782b7b20602c76
Adnan-Alfadhli <adnan2000611@gmail.com>
2022-04-01 19:54:38 +0300

    init

---
+++ buyIt/buyIt/buyItApp.swift
@@ -0,0 +1,17 @@
+//
+//  buyItApp.swift
+//  buyIt
+//
+//  Created by Adnan Alfadhli on 01/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct buyItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
